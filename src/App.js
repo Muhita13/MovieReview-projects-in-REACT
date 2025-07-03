73dc3ede4fdb2ddb1bbd05910aa8764534293aa1
muhitasenthil13@gmail.com <muhitasenthil13@gamil.com>
2025-07-03 12:22:27 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import GiveFeedback from "./components/GiveFeedback";
+import ReviewList from "./components/ReviewList";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <h1>🎬 Movie Feedback App</h1>
+        <nav className="nav">
+          <Link to="/feedback">Give Feedback</Link>
+          <Link to="/reviews">View Reviews</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Navigate to="/feedback" />} />
+          <Route path="/feedback" element={<GiveFeedback />} />
+          <Route path="/reviews" element={<ReviewList />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
